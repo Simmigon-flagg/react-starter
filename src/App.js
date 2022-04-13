153942a395daf7f78c8c191cc6cb7a10e7550b5c
Simmigon Flagg <Simmigon.Flagg@macys.com>
2022-04-13 19:21:17 -0400

    Updated Template. Added Component for copy paste

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 const App = () => {
-  return <div>React Starter code</div>;
+  return <div>React Starter Code</div>;
 };
 
 export default App;
---
+++ src/components/Component/index.js
@@ -0,0 +1,7 @@
+import "./style.css"
+
+const Component = () => {
+    return (<p>Component</p>)
+}
+
+export default Component;
